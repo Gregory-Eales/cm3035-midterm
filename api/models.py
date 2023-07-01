f924e70b5648fbdb9e8a7e9202c23a002a2edd58
Gregory H Eales <gregory.hamilton.e@gmail.com>
2023-07-01 13:43:44 -0700

    add db models

--- api/models.py
+++ api/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Organism(models.Model):
+    genus_name = models.CharField(max_length=255)
+    species_name = models.CharField(max_length=255)
+
+
 class Protein(models.Model):
-    
-    pass
-
-class Pfam(models.Model):
-        
-        pass    
+    organism = models.ForeignKey(Organism, on_delete=models.CASCADE)
+    protein_sequence = models.TextField()
 
 
+class Domain(models.Model):
+    domain_description = models.TextField()
+
+
+class ProteinDomain(models.Model):
+    protein = models.ForeignKey(Protein, on_delete=models.CASCADE)
+    domain = models.ForeignKey(Domain, on_delete=models.CASCADE)
+    start_coordinate = models.IntegerField()
+    end_coordinate = models.IntegerField()
