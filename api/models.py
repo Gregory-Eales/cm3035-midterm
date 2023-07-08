48143004f87513c8e4692f5f84830f56cf822d2b
Gregory H Eales <gregory.hamilton.e@gmail.com>
2023-07-07 21:37:49 -0700

    add data models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+# I wrote this code
+
+
+class Organism(models.Model):
+    tax_id = models.IntegerField(primary_key=True)
+    clade = models.TextField(blank=True)
+    scientific_name = models.TextField()
+
+
+class Protein(models.Model):
+    protein_id = models.CharField(max_length=200, primary_key=True)
+    sequence = models.TextField(blank=True)
+    length = models.IntegerField()
+    organism = models.ForeignKey(Organism, on_delete=models.CASCADE)
+
+
+class Domain(models.Model):
+    domain_id = models.CharField(max_length=200, primary_key=True)
+    description = models.TextField()
+    start_coordinate = models.IntegerField()
+    end_coordinate = models.IntegerField()
+    protein = models.ForeignKey(Protein, on_delete=models.CASCADE)
+
+
+# end of code I wrote
