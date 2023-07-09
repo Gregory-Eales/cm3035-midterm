89c861abbf2519377b95830039dac7671bbb0ffd
Gregory H Eales <gregory.hamilton.e@gmail.com>
2023-07-09 11:39:01 -0700

    modified data models

--- api/models.py
+++ api/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 class Organism(models.Model):
     taxa_id = models.IntegerField(primary_key=True)
-    clade = models.TextField(blank=True)
+    clade_id = models.TextField(blank=True)
     genus_species = models.TextField()
 
 
@@ -15,15 +15,35 @@ class Protein(models.Model):
     protein_id = models.CharField(max_length=200, primary_key=True)
     sequence = models.TextField(blank=True)
     length = models.IntegerField()
-    organism = models.ForeignKey(Organism, on_delete=models.CASCADE)
 
 
 class Domain(models.Model):
     domain_id = models.CharField(max_length=200, primary_key=True)
     description = models.TextField()
+
+
+class ProteinDomain(models.Model):
+
+    domain_id = models.ForeignKey(Domain, on_delete=models.CASCADE)
+    protein_id = models.ForeignKey(Protein, on_delete=models.CASCADE)
     start_coordinate = models.IntegerField()
     end_coordinate = models.IntegerField()
-    protein = models.ForeignKey(Protein, on_delete=models.CASCADE)
 
+    # make domain_id and protein_id indexed
+    class Meta:
+        indexes = [
+            models.Index(fields=['domain_id', 'protein_id'])
+        ]
+    
 
+class OrganismProtein(models.Model):
+    taxa_id = models.ForeignKey(Organism, on_delete=models.CASCADE)
+    protein_id = models.ForeignKey(Protein, on_delete=models.CASCADE)
+
+    # make taxa_id and protein_id indexed
+    class Meta:
+        indexes = [
+            models.Index(fields=['taxa_id', 'protein_id'])
+        ]
+    
 # end of code I wrote
